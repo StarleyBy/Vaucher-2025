7a865e6ce21612131c7d36c2e80a93b5b59fe7b9
StarleyBy <Starleyby@gmail.com>
2025-08-30 00:12:52 +0300

    Delete Vaucher directory

--- Vaucher/config/google-config.js
+++
@@ -1,2 +0,0 @@
-const GOOGLE_CLIENT_ID = '399222378656-jr2tbed8mlclebr6hjg7s8banfdcvfb7.apps.googleusercontent.com';
-const WEB_APP_URL = 'YOUR_WEB_APP_URL'; // Замените на URL вашего веб-приложения после развертывания
